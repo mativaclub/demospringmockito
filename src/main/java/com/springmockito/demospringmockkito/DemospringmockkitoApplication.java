64d8033f8773fd60b41d33c64b885e0153a582b6
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-04-27 09:11:55 +0300

    Initial commit

---
+++ src/main/java/com/springmockito/demospringmockkito/DemospringmockkitoApplication.java
@@ -0,0 +1,13 @@
+package com.springmockito.demospringmockkito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringmockkitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringmockkitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmockito/demospringmockkito/DemospringmockkitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmockito.demospringmockkito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringmockkitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
